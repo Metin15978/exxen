0cc616bb1c0046f4ad3dd1008d4d8f3d3547ee07
Metin15978 <155890451+Metin15978@users.noreply.github.com>
2024-05-20 16:02:22 +0300

    Add files via upload

---
+++ exxen/movies/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+    isim = models.CharField(max_length=200)
+    resim = models.FileField(upload_to='filmler/',verbose_name='Film Resmi')
+    
+
+    def __str__(self):
+        return self.isim
